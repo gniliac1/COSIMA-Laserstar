e225315b4a4319cfdaeaadc6156e55420360599d
christophBehr <diagnoseschmerz@web.de>
2018-08-10 19:17:29 +0200

    BluetoothExperiments

--- Elektronik/serialCommunication/serialComPython.py
+++ Elektronik/serialCommunication/serialComPython.py
@@ -1,26 +1,19 @@
 import serial
-import time
 
 bt = serial.Serial('COM5', 9600, timeout = .1) #COMx is used COM-Port x must be checked on PC
-mod = input('Modus? r(read), s(send)')
+mod = input('Modus? r(read), e(exit)')
+print(mod)
+while True:
+    data = bt.readline()[:-2]
+    
+    if mod== 'r':
+        print(data)
 
-if mod == 'r':
-    while True:
-        data = bt.readline()[:-2]
-        input = input('>> ')
-
-        if data:
-            print(data)
-
-        if input == 'exit':
-            bt.close()
-            exit()
-        else:
-            bt.write(input + '\r\n')
-            out = ''
-            time.sleep(1)
-            while bt.inWaiting() > 0:
-                out += bt.read(1)
-                if out != '':
-                    print ('>>' + out)
+    if mod == 'e':
+        bt.close()
+                    
+    else:
+        print('wrong choice, closing connection')
+        bt.close()
+            
 
