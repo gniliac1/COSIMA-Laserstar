80310e1efe6c238b353f757595344ded8f770925
christophBehr <diagnoseschmerz@web.de>
2018-08-13 14:44:17 +0200

    removed mistake(s)

--- Elektronik/bluetoothSensor/bluetoothSensor.ino
+++ Elektronik/bluetoothSensor/bluetoothSensor.ino
@@ -24,25 +24,25 @@ void setup() {
   int16_t adc30, adc31, adc32, adc33;
   
   //First mux
-  adc00 = ads1015.readADC_SingleEnded(0);
-  adc01 = ads1015.readADC_SingleEnded(1);
-  adc02 = ads1015.readADC_SingleEnded(2);
-  adc03 = ads1015.readADC_SingleEnded(3);
+  adc00 = mux0.readADC_SingleEnded(0);
+  adc01 = mux0.readADC_SingleEnded(1);
+  adc02 = mux0.readADC_SingleEnded(2);
+  adc03 = mux0.readADC_SingleEnded(3);
   //Second mux
-  adc10 = ads1015.readADC_SingleEnded(0);
-  adc11 = ads1015.readADC_SingleEnded(1);
-  adc12 = ads1015.readADC_SingleEnded(2);
-  adc13 = ads1015.readADC_SingleEnded(3);
+  adc10 = mux1.readADC_SingleEnded(0);
+  adc11 = mux1.readADC_SingleEnded(1);
+  adc12 = mux1.readADC_SingleEnded(2);
+  adc13 = mux1.readADC_SingleEnded(3);
   //Third mux
-  adc20 = ads1015.readADC_SingleEnded(0);
-  adc21 = ads1015.readADC_SingleEnded(1);
-  adc22 = ads1015.readADC_SingleEnded(2);
-  adc23 = ads1015.readADC_SingleEnded(3);
+  adc20 = mux2.readADC_SingleEnded(0);
+  adc21 = mux2.readADC_SingleEnded(1);
+  adc22 = mux2.readADC_SingleEnded(2);
+  adc23 = mux2.readADC_SingleEnded(3);
   //Fourth mux
-  adc30 = ads1015.readADC_SingleEnded(0);
-  adc31 = ads1015.readADC_SingleEnded(1);
-  adc32 = ads1015.readADC_SingleEnded(2);
-  adc33 = ads1015.readADC_SingleEnded(3);
+  adc30 = mux3.readADC_SingleEnded(0);
+  adc31 = mux3.readADC_SingleEnded(1);
+  adc32 = mux3.readADC_SingleEnded(2);
+  adc33 = mux3.readADC_SingleEnded(3);
   //Start serial / i2c communication
   mux0.begin;
   mux1.begin;
@@ -55,43 +55,43 @@ void setup() {
 void loop() {
   //Read values from mux0 - mux3 and send them via bluetooth to python 
   Wire.requestFrom(mux0, 16);
-  bt.print("Value0 = ");
+  bt.print("Value00 = ");
   bt.println(adc00);
-  bt.print("Value0 = ");
+  bt.print("Value01 = ");
   bt.println(adc01);
-  bt.print("Value0 = ");
+  bt.print("Value02 = ");
   bt.println(adc02);
-  bt.print("Value0 = ");
+  bt.print("Value03 = ");
   bt.println(adc03);
   
   Wire.requestFrom(mux1, 16);
-  bt.print("Value0 = ");
+  bt.print("Value10 = ");
   bt.println(adc10);
-  bt.print("Value0 = ");
+  bt.print("Value11 = ");
   bt.println(adc11);
-  bt.print("Value0 = ");
+  bt.print("Value12 = ");
   bt.println(adc12);
-  bt.print("Value0 = ");
+  bt.print("Value13 = ");
   bt.println(adc13);
   
   Wire.requestFrom(mux2, 16);
-  bt.print("Value0 = ");
+  bt.print("Value20 = ");
   bt.println(adc20);
-  bt.print("Value0 = ");
+  bt.print("Value21 = ");
   bt.println(adc21);
-  bt.print("Value0 = ");
+  bt.print("Value22 = ");
   bt.println(adc22);
-  bt.print("Value0 = ");
+  bt.print("Value23 = ");
   bt.println(adc23);
   
   Wire.requestFrom(mux3, 16);
-  bt.print("Value0 = ");
+  bt.print("Value30 = ");
   bt.println(adc30);
-  bt.print("Value0 = ");
+  bt.print("Value31 = ");
   bt.println(adc31);
-  bt.print("Value0 = ");
+  bt.print("Value32 = ");
   bt.println(adc32);
-  bt.print("Value0 = ");
+  bt.print("Value33 = ");
   bt.println(adc33)
   
 }
