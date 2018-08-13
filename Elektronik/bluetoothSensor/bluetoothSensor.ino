d6e909296abe926fec52d430f16b0063c38df799
christophBehr <diagnoseschmerz@web.de>
2018-08-13 14:27:41 +0200

    Implemented function for OPA gain control

--- Elektronik/bluetoothSensor/bluetoothSensor.ino
+++ Elektronik/bluetoothSensor/bluetoothSensor.ino
@@ -9,6 +9,13 @@ Adafruit_ADS1015 mux3(0x1001011);
 SoftwareSerial bt(7,6); //RX, TX
 
 void setup() {
+  //Set Gain level for OPA
+  /*ads1015.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV (default)
+  ads1015.setGain(GAIN_ONE);     // 1x gain   +/- 4.096V  1 bit = 2mV
+  ads1015.setGain(GAIN_TWO);     // 2x gain   +/- 2.048V  1 bit = 1mV
+  ads1015.setGain(GAIN_FOUR);    // 4x gain   +/- 1.024V  1 bit = 0.5mV
+  ads1015.setGain(GAIN_EIGHT);   // 8x gain   +/- 0.512V  1 bit = 0.25mV
+  ads1015.setGain(GAIN_SIXTEEN); // 16x gain  +/- 0.256V  1 bit = 0.125mV*/
   
   //Initaialize 16bit signal for each line on each mux
   int16_t adc00, adc01, adc02, adc03;
