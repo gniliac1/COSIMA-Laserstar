5caa1c27ff370bffacb464b79ef28563f7a58152
Daniel Wolff <daniel.wolff@rwth-aachen.de>
2018-09-20 11:37:49 +0200

    renamed directory according to convention of arduino ide

---
+++ Elektronik/i2cSensorRead/i2cSensorSendIfConstant/i2cSensorSendIfConstant.ino
@@ -0,0 +1,109 @@
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>                                                
+
+//Initialize I2C addresses of multiplexers
+Adafruit_ADS1015 mux0(0x48);
+Adafruit_ADS1015 mux1(0x49);
+Adafruit_ADS1015 mux2(0x4A);
+Adafruit_ADS1015 mux3(0x4B);
+
+SoftwareSerial bt(7,6); //RX, TX
+
+const int16_t nPhotodiodes = 16;  // number of photodiodes
+int16_t adc[nPhotodiodes];        // global array for photo current
+bool valueChanged;                // indicates whether the photo current has changed compared to the previous iteration
+bool changesWritten;              // ndcates whether the photo currents have already been written to the serial port
+const int16_t threshold = 3;      // threshold by which the values have to be changed before they are communicated
+int16_t temp;                     // temporary variable for storing the photo current
+int16_t counter = 0;              // contains the current iteration of the loop
+
+void setup() {
+  
+  //Set Gain level for OPA
+  //mux0.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV (default)
+  //mux0.setGain(GAIN_ONE);     // 1x gain   +/- 4.096V  1 bit = 2mV
+  //mux0.setGain(GAIN_TWO);     // 2x gain   +/- 2.048V  1 bit = 1mV
+  mux0.setGain(GAIN_FOUR);    // 4x gain   +/- 1.024V  1 bit = 0.5mV
+  //mux0.setGain(GAIN_EIGHT);   // 8x gain   +/- 0.512V  1 bit = 0.25mV
+  //mux0.setGain(GAIN_SIXTEEN); // 16x gain  +/- 0.256V  1 bit = 0.125mV*/
+  
+  Serial.begin(9600);
+  mux0.begin();
+  mux1.begin();
+  mux2.begin();
+  mux3.begin();
+
+  changesWritten = false;
+
+}
+
+void loop() {
+  
+  // no value has changed yet
+  valueChanged = false;
+
+  // check for changes in mux0
+  for(int16_t iDiode=0; iDiode < 4; ++iDiode) {
+    temp = mux0.readADC_SingleEnded(iDiode);
+    if( abs(temp - adc[iDiode]) >= threshold ) {
+      valueChanged = true;
+      adc[iDiode] = temp;
+    }
+  }
+
+  /*// check for changes in mux1
+  for(int16_t iDiode=0; iDiode < 4; ++iDiode) {
+    temp = mux1.readADC_SingleEnded(iDiode);
+    if( temp != adc[iDiode+4] ) {
+      valueChanged = true;
+      adc[iDiode+4] = temp;
+    }
+  }
+
+  // check for changes in mux0
+  for(int16_t iDiode=0; iDiode < 4; ++iDiode) {
+    temp = mux2.readADC_SingleEnded(iDiode);
+    if( temp != adc[iDiode+8] ) {
+      valueChanged = true;
+      adc[iDiode+8] = temp;
+    }
+  }
+
+  // check for changes in mux0
+  for(int16_t iDiode=0; iDiode < 4; ++iDiode) {
+    temp = mux3.readADC_SingleEnded(iDiode);
+    if( temp != adc[iDiode+12] ) {
+      valueChanged = true;
+      adc[iDiode+12] = temp;
+    }
+  }*/
+
+  // only send data, if no value has changed and if the values have not yet been written to the stream
+  if(!valueChanged && !changesWritten) {
+
+    // for readability issues
+    Serial.println(counter);
+    Serial.println("-----");
+
+    // send data to the serial port
+    for(int16_t iDiode=0; iDiode<4; ++iDiode) {
+      Serial.print(iDiode); Serial.print(" "); Serial.println(adc[iDiode]);
+    }
+    Serial.println(" ");
+
+    // values have been written
+    changesWritten = true;
+
+  }
+
+  // if the values have been currently changed, these changes have not yet been written to the serial port
+  if(valueChanged) {
+    changesWritten = false;
+  }
+  
+  ++counter;
+  
+  delay(100);
+  
+}
