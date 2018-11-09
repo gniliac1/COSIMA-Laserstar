fd2978613e9a96ae9ffea70111d1a9ad11e5c5fb
Cailing Fu <cailingfu@hotmail.de>
2018-11-09 23:43:15 +0100

    Platinen Test Code

--- Elektronik/Adafruit_TCA9548A_1-to-8_I2C_Multiplexer_Breakout/Adafruit_TCA9548A_1-to-8_I2C_Multiplexer_Breakout.ino
+++ Elektronik/Adafruit_TCA9548A_1-to-8_I2C_Multiplexer_Breakout/Adafruit_TCA9548A_1-to-8_I2C_Multiplexer_Breakout.ino
@@ -5,8 +5,8 @@
 #define TCAADDR 0x70
 
 /* Assign a unique ID to this sensor at the same time */
-Adafruit_ADS1015 mux00(0x49);
-Adafruit_ADS1015 mux10(0x49);
+Adafruit_ADS1015 mux00(0x4A);
+Adafruit_ADS1015 mux10(0x4A);
 
 
 
@@ -41,15 +41,28 @@ void setup(void)
 void loop(void) 
 {
   
-  tcaselect(0);
+  tcaselect(0); // Oktokommander
   int16_t adc00 = mux00.readADC_SingleEnded(0);
+  int16_t adc01 = mux00.readADC_SingleEnded(1);
+  int16_t adc02 = mux00.readADC_SingleEnded(2);
+  int16_t adc03 = mux00.readADC_SingleEnded(3);
 
-  tcaselect(1);
+  tcaselect(1); // Detektormodul
   int16_t adc10 = mux10.readADC_SingleEnded(0);
+  int16_t adc11 = mux10.readADC_SingleEnded(1);
+  int16_t adc12 = mux10.readADC_SingleEnded(2);
+  int16_t adc13 = mux10.readADC_SingleEnded(3);
  
   /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
-  Serial.print("Sensor #1 - "); Serial.println(adc00);
-  Serial.print("Sensor #2 - "); Serial.println(adc10);
+  Serial.print("Modul 1 Sensor 0 - "); Serial.println(adc00);
+  Serial.print("Modul 1 Sensor 1 - "); Serial.println(adc01);
+  Serial.print("Modul 1 Sensor 2 - "); Serial.println(adc02);
+  Serial.print("Modul 1 Sensor 3 - "); Serial.println(adc03);
+  Serial.println("");
+  Serial.print("Modul 2 Sensor 0 - "); Serial.println(adc10);
+  Serial.print("Modul 2 Sensor 1 - "); Serial.println(adc11);
+  Serial.print("Modul 2 Sensor 2 - "); Serial.println(adc12);
+  Serial.print("Modul 2 Sensor 3 - "); Serial.println(adc13);
   Serial.println("");
   
   delay(500);
